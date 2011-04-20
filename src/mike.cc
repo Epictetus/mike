d9cf955f2cfe1cbaf469086f4db63a5c70c53c42
Chris Kowalik <chris@nu7hat.ch>
2011-04-20 01:55:52 -0300

    base autotools files + base sources

---
+++ src/mike.cc
@@ -0,0 +1,22 @@
+#include "mike.h"
+
+namespace mike
+{
+  /*
+   * Initializes mike's environment. 
+   *
+   */
+  void Initialize()
+  {
+    // ...
+  }
+
+  /*
+   * Disposes memory reserved by mike's environment. 
+   *
+   */
+  void Dispose()
+  {
+    // ...
+  }
+}
---
+++ src/mike.h
@@ -0,0 +1,16 @@
+#ifndef _MIKE_H_
+#define _MIKE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <v8.h>
+
+namespace mike
+{
+  void Initialize();
+  void Dispose();
+}
+
+#endif /* _MIKE_H_ */
