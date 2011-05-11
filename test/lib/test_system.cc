66910dbb98efda5ddf54e17e089e6865cb43b066
Chris Kowalik <dev+nu7hatch@cuboxsa.com>
2011-05-11 14:34:57 -0300

    another fixes for fuckintosh - related to cwd and pwd functions from System

--- test/lib/test_system.cc
+++ test/lib/test_system.cc
@@ -1,6 +1,8 @@
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/param.h>
 
 #include "test/macros.h"
 #include "core/context.h"
@@ -17,6 +19,7 @@ class SystemFunctionsTest : public CppUnit::TestFixture
   CPPUNIT_TEST_SUITE_END();
 private:
   context::Window *window;
+  string cwd;
 public:
   void setUp();
   void tearDown();
@@ -30,6 +33,13 @@ CPPUNIT_TEST_SUITE_REGISTRATION(SystemFunctionsTest);
 void SystemFunctionsTest::setUp(void)
 {
   window = context::New();
+  char pcwd[MAXPATHLEN];
+  getcwd(pcwd, MAXPATHLEN);
+  cwd = string(pcwd) + "/../tmp";
+  system(("mkdir -p " + cwd).c_str());
+  chdir(cwd.c_str());
+  getcwd(pcwd, MAXPATHLEN);
+  cwd = string(pcwd);
 }
 
 void SystemFunctionsTest::tearDown(void)
@@ -40,15 +50,15 @@ void SystemFunctionsTest::tearDown(void)
 void SystemFunctionsTest::pwdFuncTest()
 {
   HandleScope scope;
-  chdir("/tmp");
-  ASSERT_JS_EQUAL("System.pwd()", String::New("/tmp"));
+  ASSERT_JS_EQUAL("System.pwd()", String::New(cwd.c_str()));
 }
 
 void SystemFunctionsTest::cwdFuncTest()
 {
   HandleScope scope;
-  system("mkdir -p /tmp/testing-dir");
-  ASSERT_JS_EQUAL("System.cwd('/tmp/not-exists')", False());
-  ASSERT_JS_EQUAL("System.cwd('/tmp/testing-dir')", True());
-  ASSERT_JS_EQUAL("System.pwd()", String::New("/tmp/testing-dir"));
+  system(("mkdir -p " + cwd + "/testing-dir").c_str());
+  ASSERT_JS_EQUAL("System.cwd('not-exists')", False());
+  ASSERT_JS_EQUAL("System.cwd('testing-dir')", True());
+  ASSERT_JS_EQUAL("System.pwd()", String::New((cwd + "/testing-dir").c_str()));
+
 }
