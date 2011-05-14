9118dedf2a4658f37dd25b4b671d10b8ae37dc87
Chris Kowalik <chris@nu7hat.ch>
2011-05-14 19:41:26 -0300

    started working on time glue

---
+++ src/glue/time.cc
@@ -0,0 +1,1 @@
+#include "glue/time.h"
---
+++ src/glue/time.h
@@ -0,0 +1,13 @@
+#ifndef _MIKE_GLUE_TIME_H_
+#define _MIKE_GLUE_TIME_H_
+
+namespace mike {
+  namespace glue {
+    namespace time
+    {
+      
+    }
+  }
+}
+
+#endif /* _MIKE_GLUE_TIME_H_ */
