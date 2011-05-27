03ff5be1084d7118039873af0de7eb203df97556
Chris Kowalik <chris@nu7hat.ch>
2011-05-27 01:37:24 -0300

    some docs

--- src/mike/history.cc
+++ src/mike/history.cc
@@ -3,11 +3,17 @@
 
 namespace mike
 {
+  /**
+   * Constructor.
+   */
   History::History()
     : current(NULL)
   {
   }
-  
+
+  /**
+   * Destructor.
+   */
   History::~History()
   {
     back.push_back(current);
@@ -15,16 +21,28 @@ namespace mike
     forward.clear();
   }
 
+  /**
+   * Returns currently browsed page.
+   */
   pPage History::Current()
   {
     return current;
   }
-  
+
+  /**
+   * Returns total number of pages stored in history. 
+   */
   int History::Length()
   {
     return back.size()+forward.size();
   }
 
+  /**
+   * Loads given page and apends it to history list. At the same time it
+   * clears forward pages stack.
+   *
+   * @param page: page to open
+   */
   void History::Push(pPage page)
   {
     if (current != NULL) {
@@ -36,23 +54,39 @@ namespace mike
     current->Load();
   }
 
+  /**
+   * Goes to previous page from history stack. 
+   */
   void History::Back()
   {
     Go(-1);
   }
 
+  /**
+   * Goes to next page from history stack.
+   */
   void History::Forward()
   {
     Go(1);
   }
-  
+
+  /**
+   * Makes given number of steps through current history stack. When distance
+   * is higher than 0 then goes forward, otherwise backward.
+   *
+   *   history->Go(10); // 10 pages forward
+   *   history->Go(-2); // 2 pages backward
+   *
+   * @param distance: number of steps to go
+   */
   void History::Go(int distance)
   {
     if (distance != 0) {
       pPage it;
       PageList *from = distance > 0 ? &forward : &back;
       PageList *to = distance < 0 ? &forward : &back;
-    
+
+      // TODO: there is definitelly some other, more efficient way to do this
       while ((*from).size() > 0 && abs(distance) > 0) {
 	if (it) (*to).push_back(it);
 	it = (*from).back();
